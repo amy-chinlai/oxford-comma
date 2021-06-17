2e48a3f8b5b71374dd3507fb815f85e73b7cdbb9
amy-chinlai <amy@actionnetwork.org>
2021-06-17 14:25:09 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+    new_string = ""
+    if array.length() == 2
+        new_string = array.join(" and ")
+    else
+    new_string = array.join(", ")
+    end
+    new_string.to_s
+    new_string.sub(/.*\K,/, ', and')
 end
